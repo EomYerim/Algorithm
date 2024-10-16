0cb536ed42fce3c55aad2f8fcd91dd672cfd7937
Eom-Ye-rim <dpfladlwkdrn@naver.com>
2024-10-16 20:11:21 +0900

    [update] 우물 안 개구리.py

--- softeer/우물 안 개구리.py
+++ softeer/우물 안 개구리.py
@@ -1,18 +1,28 @@
 import sys
+from collections import defaultdict
 
 # 자신이 최고라고 생각하는 회원의 수
 N,M= map(int,input().split())
 weight = list(map(int,input().split()))
 member=list(list(map(int,input().split())) for _ in range(M))
-total_member=[i for i in range(1,N+1)]
-dic=dict()
-cnt=0
-for a,b in enumerate (weight,start=1):
-    dic[a]=b
 
-for i in member:
-    if dic[i[0]]>dic[i[1]]:
-        cnt+=1
-    if i[0] not in total_member:
-        cnt+=1
-print(cnt)
+dic=defaultdict(list)
+
+for a,b in enumerate (member,start=1):
+    dic[b[0]].append(b[1])
+    dic[b[1]].append(b[0])
+
+count = 0
+for i in range(1, N+1):
+    # 친구가 없으면 자신이 최고라고 생각
+    if not dic[i]:
+        count += 1
+    else:
+        # 친구들보다 무게를 더 많이 들 수 있으면 자신이 최고라고 생각
+        if all(weight[i-1] > weight[friend-1] for friend in dic[i]):
+            count += 1
+
+print(count)
+
+
+    
