4fb5db57d4b3aa166d6858f65a2ee913230fba31
Eom-Ye-rim <dpfladlwkdrn@naver.com>
2024-10-24 16:43:05 +0900

    [update] 백준 11053.py

--- dp/가장 긴 증가하는 부분 수열 3.py
+++ dp/가장 긴 증가하는 부분 수열 3.py
@@ -1,8 +1,23 @@
 import sys
 
-n = int(input())
-str=list(map(int,input().split()))
-dp=[0]*(n+1)
-dp[0]=str[0]
-for i in range(n):
-    dp[i]=str[0]
+N = int(input())
+arr = list(map(int,input().split()))
+
+def binary_search(arr,target):
+    start,end=0,len(arr)-1
+    while start<=end:
+        mid=(start+end)//2
+        if (arr[mid]<target):
+            start=mid+1
+        else:
+            end=mid-1
+    return start
+
+res=[arr[0]]
+for i in range(1,len(arr)):
+    if arr[i]>res[-1]:
+        res.append(arr[i])
+    else:
+        index=binary_search(res,arr[i])
+        res[index]=arr[i]
+print(len(res))
